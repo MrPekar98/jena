c88271426795a57545b4693f3d2bc0400969118e
MrPekar98 <martin.pekar@outlook.dk>
2020-11-05 18:21:42 +0100

    Uses NodeTabeNativeSplitter.java.

--- jena-tdb/src/main/java/org/apache/jena/tdb/store/nodetable/NodeTableNative.java
+++ jena-tdb/src/main/java/org/apache/jena/tdb/store/nodetable/NodeTableNative.java
@@ -39,7 +39,7 @@ import org.apache.jena.tdb.store.NodeId ;
 public class NodeTableNative implements NodeTable
 {
     protected ObjectFile objects ;
-    protected Index nodeHashToId;          // hash -> int
+    protected NodeTableNativeSplitter nodeHashToId;          // hash -> int
     private boolean syncNeeded = false ;    // Non-transactional mode sync.
     
     // Delayed construction - must call init explicitly.
@@ -54,7 +54,7 @@ public class NodeTableNative implements NodeTable
     
     protected void init(Index nodeToId, ObjectFile objectFile)
     {
-        this.nodeHashToId = nodeToId;
+        this.nodeHashToId = NodeTableNativeSplitter.buildFromIndex(nodeToId, objectFile);
         this.objects = objectFile;
     }
 
@@ -123,11 +123,11 @@ public class NodeTableNative implements NodeTable
     
     private final NodeId accessIndex(Node node, boolean create)
     {
-        Hash hash = new Hash(nodeHashToId.getRecordFactory().keyLength()) ;
+        Hash hash = new Hash(nodeHashToId.getRecordFactory(node).keyLength()) ;
         setHash(hash, node) ;
         byte k[] = hash.getBytes() ;        
         // Key only.
-        Record r = nodeHashToId.getRecordFactory().create(k) ;
+        Record r = nodeHashToId.getRecordFactory(node).create(k) ;
         
         synchronized (this)  // Pair to readNodeFromTable.
         {
